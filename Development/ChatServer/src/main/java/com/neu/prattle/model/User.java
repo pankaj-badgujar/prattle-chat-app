be49d318c11682d58effe2c8b7c5a5e0a835e22b
Devansh Gandhi <gandhi.de@husky.neu.edu>
2019-10-17 14:08:45 -0400

    Updated user model to encorporate private messages feature

--- Development/ChatServer/src/main/java/com/neu/prattle/model/User.java
+++ Development/ChatServer/src/main/java/com/neu/prattle/model/User.java
@@ -1,62 +1,122 @@
 package com.neu.prattle.model;
 
+import com.neu.prattle.exceptions.NoSuchUserPresentException;
+import com.neu.prattle.service.UserService;
+import com.neu.prattle.service.UserServiceImpl;
+
 import java.util.Objects;
+import java.util.UUID;
 
 /***
  * A User object represents a basic account information for a user.
  *
- * @author CS5500 Fall 2019 Teaching staff
- * @version dated 2019-10-06
+ * @author Devansh Gandhi
+ * @version dated 2019-10-16
  */
 public class User {
 
-	public String getName() {
-		return name;
-	}
+  private String name;
+  private final String userID;
+  private String connectedTo;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  private User() {
+    connectedTo = null;
+    this.userID = UUID.randomUUID().toString();
+  }
 
-	private String name;
+  /**
+   * A constructor using which we can create an object of the class {@link User} which takes in the
+   * name of the user.
+   *
+   * @param name The name of the user whose object needs to be created.
+   */
+  public User(String name) {
+    this.name = name;
+    this.connectedTo = null;
+    this.userID = UUID.randomUUID().toString();
+  }
 
-	public User() {
+  /**
+   * Getter method to get the name of the user attached to this object.
+   *
+   * @return Name of the user.
+   */
+  public String getName() {
+    return name;
+  }
 
-	}
+  /**
+   * Setter method to change the name of the user to the name provided in the parameter.
+   *
+   * @param name The changed name of this user.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public User(String name) {
-        this.name = name;
+  /**
+   * Getter method to get the unique user ID of this user.
+   *
+   * @return Unique user ID of the user.
+   */
+  public String getUserID() {
+    return userID;
+  }
+
+  /**
+   * Getter to get all the currently connected user to this user.
+   *
+   * @return List of the users that are connected to this user.
+   */
+  public String getConnectedUsers() {
+    return connectedTo;
+  }
+
+  /**
+   * Setter methods to connect this users to the other users passed in the parameter.
+   *
+   * @param otherUsers users that needs to be connected to this user.
+   */
+  public void connectTo(String otherUsers) {
+
+    UserService allRegisteredUsers = UserServiceImpl.getInstance();
+
+    if (!allRegisteredUsers.findUserByName(otherUsers).isPresent()) {
+      throw new NoSuchUserPresentException(otherUsers);
     }
 
-    /***
-     * Returns the hashCode of this object.
-     *
-     * As name can be treated as a sort of identifier for
-     * this instance, we can use the hashCode of "name"
-     * for the complete object.
-     *
-     *
-     * @return hashCode of "this"
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
+    this.connectedTo = otherUsers;
+  }
 
-    /***
-     * Makes comparison between two user accounts.
-     *
-     * Two user objects are equal if their name are equal ( names are case-sensitive )
-     *
-     * @param obj Object to compare
-     * @return a predicate value for the comparison.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof User))
-            return false;
+  /***
+   * Returns the hashCode of this object.
+   *
+   * As name can be treated as a sort of identifier for
+   * this instance, we can use the hashCode of "name"
+   * for the complete object.
+   *
+   *
+   * @return hashCode of "this"
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
 
-        User user = (User) obj;
-        return user.name.equals(this.name);
-    }
+  /***
+   * Makes comparison between two user accounts.
+   *
+   * Two user objects are equal if their name are equal ( names are case-sensitive )
+   *
+   * @param obj Object to compare
+   * @return a predicate value for the comparison.
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof User))
+      return false;
+
+    User user = (User) obj;
+    return user.name.equals(this.name);
+  }
 }
---
+++ Development/ChatServer/src/test/java/com/neu/prattle/UserTest.java
@@ -0,0 +1,66 @@
+package com.neu.prattle;
+
+import com.neu.prattle.exceptions.NoSuchUserPresentException;
+import com.neu.prattle.model.User;
+import com.neu.prattle.service.UserService;
+import com.neu.prattle.service.UserServiceImpl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+  @Test
+  public void testUserCreation() {
+    String name = "devansh";
+    User user = new User(name);
+    assertEquals(name, user.getName());
+  }
+
+  @Test
+  public void testUserNameChange() {
+    String name = "devansh";
+    User user = new User(name);
+    assertEquals(name, user.getName());
+    String changedName = "Devansh";
+    user.setName(changedName);
+    assertEquals(changedName, user.getName());
+  }
+
+  @Test
+  public void testUsersConnection() {
+    UserService userService = UserServiceImpl.getInstance();
+    String harshilName = "harshil";
+    User devansh = new User("Devansh");
+    User harshil = new User(harshilName);
+    userService.addUser(devansh);
+    userService.addUser(harshil);
+
+    harshil.connectTo(devansh.getName());
+
+    devansh.connectTo(harshilName);
+
+    assertEquals(devansh.getName(), harshil.getConnectedUsers());
+    assertEquals(harshilName, devansh.getConnectedUsers());
+
+    String pankajName = "Pankaj";
+    User pankaj = new User(pankajName);
+    userService.addUser(pankaj);
+
+
+    harshil.connectTo(pankajName);
+    assertEquals(pankajName, harshil.getConnectedUsers());
+  }
+
+  @Test(expected = NoSuchUserPresentException.class)
+  public void testInvalidConnectedGroups() {
+    UserService userService = UserServiceImpl.getInstance();
+    User devansh = new User("devansh");
+    userService.addUser(devansh);
+
+    // We are adding an invalid user which is yet not registered which should throw an exception
+    devansh.connectTo("harshil1");
+  }
+
+}
